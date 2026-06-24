091fdb7946a2a93d4746fdf04dd4ddbfb3ae7bf8
ChaniDev <ChaniDev@mandel.org.il>
2026-06-24 18:10:18 +0300

    Add custom fonts

---
+++ PhoneList/src/webparts/employeesDirectory/components/IEmployeesDirectoryProps.ts
@@ -0,0 +1,7 @@
+export interface IEmployeesDirectoryProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ PhoneList/src/webparts/employeesDirectory/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEmployeesDirectoryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EmployeesDirectoryWebPartStrings' {
+  const strings: IEmployeesDirectoryWebPartStrings;
+  export = strings;
+}
