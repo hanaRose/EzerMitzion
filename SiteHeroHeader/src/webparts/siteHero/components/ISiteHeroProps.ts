a8a087af1d272b7f9ce1fa3aab109b21e81af6d3
yaelifrommer <yaelifrommer1@gmail.com>
2026-09-17 15:51:04 +0300

    feat: make site hero configurable per page

--- SiteHeroHeader/src/webparts/siteHero/components/ISiteHeroProps.ts
+++ SiteHeroHeader/src/webparts/siteHero/components/ISiteHeroProps.ts
@@ -1,8 +1,5 @@
- import { WebPartContext } from "@microsoft/sp-webpart-base";
-
 export interface ISiteHeroProps {
-  context: WebPartContext;
-  listName: string;
-  overrideHeroText?: string;
-  overrideSubTitle?: string;
+  heroText?: string;
+  subTitle?: string;
+  backgroundImageUrl?: string;
 }
